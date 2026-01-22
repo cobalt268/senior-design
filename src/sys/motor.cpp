cf930a904f97b43e6b5e66d5d24d64b37ba59d96
Will Stotz <cobaltisacoolore@gmail.com>
2026-01-22 14:30:28 -0500

    more work on forward drive. figure out constants experimentally...

--- src/sys/motor.cpp
+++ src/sys/motor.cpp
@@ -3,7 +3,8 @@
 #include "../hal/motor_hal.h"
 #include "../hal/encoder_hal.h"
 
-#define LEFT_RIGHT_RATIO 1
+#define LEFT_RIGHT_RATIO 1      //todo fix this
+#define COUNTS_PER_MM 5         //todo fix this
 
 /// @brief configures motor pwm and encoders
 /// @param
@@ -75,13 +76,14 @@ void proceed_forward(uint16_t mm)
     // briefly high initial torque
     left_set_forward(100.0);
     right_set_forward(100.0);
-    vTaskDelay(20 / portTICK_PERIOD_MS);
+    vTaskDelay(5 / portTICK_PERIOD_MS);
 
     // running speed
     left_set_forward(20.0);
     right_set_forward(20.0);
 
-    vTaskDelay(mm * 4.5 / portTICK_PERIOD_MS);  //TODO REPLACE THIS WITH SOME KINDA ENCODER SENSY THING
+    uint8_t start_count = get_right_encoder_count();
+    while ((get_right_encoder_count()-start_count)<mm*COUNTS_PER_MM);
 
     left_stop();
     right_stop();
